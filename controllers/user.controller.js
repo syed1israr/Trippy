fa40fc2721a58cde4aee9368ecfc96c0df65516f
Your Name <you@example.com>
2024-08-31 00:04:08 +0530

    add changeCurrentPassword  refreshAccessToken

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -153,15 +153,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 })
 
 
-const getCurrentUser = asyncHandler(async(req, res) => {
-  return res
-  .status(200)
-  .json(new ApiResponse(
-      200,
-      req.user,
-      "User fetched successfully"
-  ))
-})
+
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
